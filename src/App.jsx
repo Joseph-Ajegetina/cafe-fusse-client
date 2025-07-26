edf8b6f5f84af0a177b6111597529686ef251267
Joseph-Ajegetina <josephajongbah@gmail.com>
2025-07-26 02:30:29 +0000

    Add tanstack router for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import {Accordion, AccordionItem} from "@heroui/react";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import { StrictMode } from "react";
 
-export default function App() {
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+const router = createRouter({ routeTree });
 
+const App = () => {
   return (
-    <Accordion>
-      <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-        {defaultContent}
-      </AccordionItem>
-      <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
-        {defaultContent}
-      </AccordionItem>
-    </Accordion>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
   );
-}
+};
+
+export default App;
